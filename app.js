a4afbccb7b30b7e3b8f3efdd1ad72f74941d8d3d
Torrey Meade <themeadeconcept@hotmail.com>
2021-09-24 03:58:05 -0400

    Installed bootstrap, created started scripts and messed around with Pug

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
-const express = require("express");
-const chalk = require("chalk");
-const debug = require("debug")("app");
-const morgan = require("morgan");
-const path = require("path");
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(morgan("combined"));
+// MIDDLEWARE
+
+app.use(morgan('tiny'));
+// static directory for static files - pointing to the "public" directory
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
+);
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 // The way Express works, is that everything works through app
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+app.get('/', (req, res) => {
+  res.render('index', { list: ['a', 'b'] });
 });
 
-app.listen(3000, function () {
-  debug(`Listening on Port ${chalk.green("3000")}`);
+app.listen(port, () => {
+  debug(`Listening on Port ${chalk.green(port)}`);
 });
